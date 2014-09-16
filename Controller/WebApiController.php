3afa3a6c54b347325d55ab052b855edba9f74bf2
Muhammad Safraz <mr.safraz@gmail.com>
2014-09-16 11:29:18 +0530

    Update the implementation of WebApiController

--- Controller/WebApiController.php
+++ Controller/WebApiController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Adrotec\BreezeJs\MetadataInterceptor;
 use Adrotec\BreezeJs\Serializer\MetadataInterceptor as SerializerInterceptorBase;
 use Adrotec\BreezeJs\Validator\ValidatorInterceptor;
+use Symfony\Component\HttpFoundation\Request;
 
 //use Symfony\Component\Validator\Validator;
 
@@ -31,38 +32,15 @@ class WebApiController extends Controller {
 
     public function apiAction($route) {
         
-        $response = null;
-        /* @var $serializer \JMS\Serializer\Serializer */
-        $serializer = $this->container->get('serializer');
-        $validator = $this->container->get('validator');
+        $app = $this->container->get('adrotec_webapi');
+
+        $app->addResources($this->getClientClasses());
+
+        $request = Request::createFromGlobals();
+        $request->attributes->set('resource', $route);
+        $response = $app->handle($request);
         
-        $interceptor = new MetadataInterceptor();
-        $interceptor->add(new SerializerInterceptor($serializer));
-        $interceptor->add(new ValidatorInterceptor($validator));
-            
-        $dispatcher = new Dispatcher($this->getDoctrine()->getManager(), $interceptor);
-        
-        $classes = $this->getClientClasses();
-        
-        $dispatcher->setClasses($classes);
-        
-        if ($route == 'Metadata') {
-            $response = $dispatcher->getMetadata();
-        }
-        else if ($route == 'SaveChanges') {
-            if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-                throw new \Exception('Method not allowed');
-            }
-            $input = file_get_contents('php://input');
-            $response = $dispatcher->saveChanges($input);
-        }
-        else {
-            $response = $dispatcher->getResults($route, $_GET);
-        }
-        
-        $response = $serializer->serialize($response, 'json');
-        
-        return new Response($response);
+        return $response;
     }
 
 }
