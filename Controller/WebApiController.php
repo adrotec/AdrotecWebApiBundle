cbf90e2ed862011d9763053d12ede88d15d89a3d
Muhammad Safraz Razik <mr.safraz@gmail.com>
2014-10-20 18:53:59 +0530

    update WebApiController

--- Controller/WebApiController.php
+++ Controller/WebApiController.php
@@ -4,40 +4,25 @@ namespace Adrotec\WebApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use Adrotec\BreezeJs\Doctrine\ORM\Dispatcher;
-use Symfony\Component\HttpFoundation\Response;
-use Adrotec\BreezeJs\MetadataInterceptor;
-use Adrotec\BreezeJs\Serializer\MetadataInterceptor as SerializerInterceptorBase;
-use Adrotec\BreezeJs\Validator\ValidatorInterceptor;
 use Symfony\Component\HttpFoundation\Request;
-
-//use Symfony\Component\Validator\Validator;
-
-class SerializerInterceptor extends SerializerInterceptorBase {
-    public function getDefaultResourceName(\Adrotec\BreezeJs\Metadata\StructuralType $structuralType){
-        return $structuralType->shortName;
-    }
-}
-
-/**
- * @deprecated use the "adrotec_webapi" service instead
- */
 
 class WebApiController extends Controller {
 
-    public function getClientClasses() {
+    public function getResources() {
         //throw new \Exception('getClientClasses method should be implemented by sub classes');
         return null;
     }
 
-    public function apiAction($route) {
+    public function apiAction(Request $request) {
         
         $app = $this->container->get('adrotec_webapi');
 
-        $app->addResources($this->getClientClasses());
+        $resources = $this->getResources();
+        
+        if($resources){
+            $app->addResources($resources);
+        }
 
-        $request = Request::createFromGlobals();
-        $request->attributes->set('resource', $route);
         $response = $app->handle($request);
         
         return $response;
